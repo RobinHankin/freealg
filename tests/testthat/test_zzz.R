716aa9610f31c39f09734d9c37c5fc13d50eda3a
RobinHankin <hankin.robin@gmail.com>
2019-09-20 13:12:01 +1200

    couple of extra tests

--- tests/testthat/test_zzz.R
+++ tests/testthat/test_zzz.R
@@ -3,6 +3,7 @@
 
 test_that("Test suite zzz, specific identities and miscellaneous checks",{
 
+  expect_true(is_ok_free(list(),coeffs=1))
   expect_true(
       freealg(list(1,c(1,2)),c(4,5))==freealg(list(1,c(1,0,2)),c(4,5))
   )
@@ -12,6 +13,8 @@ test_that("Test suite zzz, specific identities and miscellaneous checks",{
   expect_silent(x <- freealg(list(1,c(1,0,2)),c(4,5)))
 
   expect_true(is.constant(as.freealg("7")))
+  expect_error(as.freealg(sin))
+
 
   x <- rfalg()
   expect_silent(x <- as.freealg(x))
@@ -21,6 +24,8 @@ test_that("Test suite zzz, specific identities and miscellaneous checks",{
 
   coeffs(x) <- 3
   expect_true(all(coeffs(x)==3))
+
+  print(x)
 
 
   })
