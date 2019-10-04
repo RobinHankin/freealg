1a62d35cb76d284014e0b0e7a729f8b2872f0d7a
RobinHankin <hankin.robin@gmail.com>
2019-10-05 11:43:14 +1300

    another test

--- tests/testthat/test_aae.R
+++ tests/testthat/test_aae.R
@@ -5,5 +5,9 @@ test_that("Test suite aae.R",{
     jj <- list(indices = list(c(-5, -5, 5, 7), c(-5, 7)), coeffs = c(-2,2))
     x <- freealg(jj[[1]],jj[[2]])
     expect_true(x==x)
+
+    expect_true(
+        "1+x+xy" %>% subs(x="1+z") %>% horner(seq_len(5)) == 
+        "1+x+xy" %>% horner(seq_len(5)) %>% subs(x="1+z"))
  
 })  # test_that() function closes
