ec5f53f6d12be8f4b267b5ba60285ce84207f7cb
RobinHankin <hankin.robin@gmail.com>
2019-10-04 15:48:15 +1300

    new tests

---
+++ tests/testthat/test_aad.R
@@ -0,0 +1,23 @@
+
+
+test_that("test suite aaa",{
+  subs("a",a="1+x") == as.freealg("1+x")^1
+  subs("aa",a="1+x") == as.freealg("1+x")^2
+  subs("aaa",a="1+x") == as.freealg("1+x")^3
+  subs("aaaa",a="1+x") == as.freealg("1+x")^4
+
+  f <- function(n){expect_true(subs(as.freealg(paste(rep("a",n),collapse="")),a="1+x") == as.freealg("1+x")^n)}
+  sapply(1:20,f)
+
+
+
+  foo <- function(p){
+    if(!24 %in% unique(c(words(p),recursive=TRUE))){
+      expect_true(subs(p,x="1+x") == p)
+    }
+
+    for(i in 1:10){
+      foo(rfalg(distinct=3))
+    }
+
+})
