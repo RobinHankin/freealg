cf372f6d5ebc2aab2d371d1fe5a3b47bd2b53141
Robin Hankin <hankin.robin@gmail.com>
2021-08-13 13:37:18 +1200

    better colour

--- inst/freealg_stickermaker.R
+++ inst/freealg_stickermaker.R
@@ -1,5 +1,5 @@
 library("hexSticker")
 
-sticker("anthony_blue.png", package="freealg", p_size=8, s_x=1, s_y=1,
-s_width=1.2, asp=0.85, white_around_sticker=TRUE, h_fill="#7733FF",
+sticker("annabel.png", package="freealg", p_size=8, s_x=1, s_y=1,
+s_width=1, asp=0.85, white_around_sticker=TRUE, h_fill="#7733FF",
 h_color="#000000", filename="freealg.png")
