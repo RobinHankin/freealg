d1c0fe6a3fcc22765cdb88162fdcae5dbe258c3d
Robin Hankin <hankin.robin@gmail.com>
2021-09-20 21:05:43 +1200

    color balance

--- inst/freealg_stickermaker.R
+++ inst/freealg_stickermaker.R
@@ -1,5 +1,5 @@
 library("hexSticker")
 
-sticker("annabel.png", package="freealg", p_size=8, s_x=1, s_y=1,
-s_width=1, asp=0.85, white_around_sticker=TRUE, h_fill="#7733FF",
+sticker("annabel.png", package="freealg", p_size=24, s_x=1, s_y=1,
+s_width=1, asp=sqrt(3)/2, white_around_sticker=TRUE, h_fill="#7733FF",
 h_color="#000000", filename="freealg.png")
