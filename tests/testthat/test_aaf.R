7df12fe1c956e94d116ede7f8774bb77c340eb98
Robin Hankin <hankin.robin@gmail.com>
2022-05-12 21:36:53 +1200

    test .[x,y]

---
+++ tests/testthat/test_aaf.R
@@ -0,0 +1,48 @@
+## This file follows the structure of aaa.R in the free group package.
+
+## Define some checker functions, and call them at the end.  They
+## should all return TRUE if the package works, and stop with error if
+## a test is failed.  Function checker1() has one argument, checker2()
+## two, and checker3() has three.  
+
+test_that("Test suite aaf.R",{
+
+checker1 <- function(x){
+    expect_error(.[])
+    expect_error(.[x])
+    expect_error(.[x,])
+    expect_error(.[,x])
+
+    expect_true(.[x,x] == 0)
+    expect_true(.[x,x*3] == 0)
+    return(TRUE)
+}  # checker1() closes
+
+
+checker2 <- function(x,y,n){
+    expect_true(.[x,y] + .[y,x] == 0)
+    expect_true(n*.[x,y] == .[n*x,y])
+    expect_true(n*.[x,y] == .[x,n*y])
+    expect_true(.[x+y,x-y] == .[y,x]*2)
+    
+    return(TRUE)
+} # checker2() closes
+
+checker3 <- function(x,y,z){
+    expect_true(jacobi(x,y,z) == 0)
+    expect_true(.[x,.[y,z]] + .[y,.[z,x]] + .[z,.[x,y]] == 0)
+}
+
+for(i in 1:2){
+    for(inc in c(TRUE,FALSE)){
+        x <- rfalg(5,include.negative=inc)
+        y <- rfalg(5,include.negative=inc)
+        z <- rfalg(5,include.negative=inc)
+        n <- sample(1:9,1)
+        checker1(x)
+        checker2(x,y,n)
+        checker3(x,y,z)
+    }
+}
+
+})
