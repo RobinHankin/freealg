5587e4e39bfc0067f5ecbdda9a4d59c086d4687f
Robin Hankin <hankin.robin@gmail.com>
2023-07-03 09:28:49 +1200

    various style suggestions made by cppcheckR

--- src/free.cpp
+++ src/free.cpp
@@ -83,7 +83,7 @@ freealg prepare(const List words, const NumericVector coeffs){
     return out;
 }
 
-word concatenate(word w1, const word w2){ 
+word concatenate(word w1, const word& w2){ 
     word::const_iterator it;
     for(it=w2.begin() ; it != w2.end() ; it++){
         w1.push_back(*it);
@@ -91,8 +91,7 @@ word concatenate(word w1, const word w2){
      return comb(w1);
 }
 
-freealg sum(freealg X1, const freealg X2){ //X1 modified in place
-    freealg out;
+freealg sum(freealg X1, const freealg& X2){ //X1 modified in place
     freealg::const_iterator it;
     for(it=X2.begin() ; it != X2.end() ; ++it){
         X1[it->first] += it->second;  // the meat
@@ -100,7 +99,7 @@ freealg sum(freealg X1, const freealg X2){ //X1 modified in place
     return X1;
 }
 
-freealg product(const freealg X1, const freealg X2){
+freealg product(const freealg& X1, const freealg& X2){
     freealg out;
     freealg::const_iterator it1,it2;
     for(it1=X1.begin() ; it1 != X1.end() ; ++it1){
@@ -111,7 +110,7 @@ freealg product(const freealg X1, const freealg X2){
     return out;
 }
 
-freealg power(const freealg X, int n){
+freealg power(const freealg& X, int n){
     freealg out; // empty freealg object is the zero object
     if(n<1){throw std::range_error("power cannot be <1");} 
     if(n==1){
@@ -125,7 +124,7 @@ freealg power(const freealg X, int n){
     return out;
 }
 
-freealg diff1(const freealg X, const int r){  // dX/dx_r
+freealg diff1(const freealg& X, const int r){  // dX/dx_r
     freealg out; // empty freealg object is the zero object
 
     for(freealg::const_iterator it=X.begin() ; it != X.end() ; ++it){
@@ -172,7 +171,7 @@ freealg diffn(freealg X, const NumericVector r){ // (d^len(r) X)/dr[1]...dr[len(
     return X;
 }
 
-freealg multiply_pre_and_post(const freealg Y, const word left, const word right){
+freealg multiply_pre_and_post(const freealg& Y, const word& left, const word& right){
 
     freealg out;
 
@@ -224,7 +223,7 @@ freealg change_r_for_zero(const freealg &X, const int &r){
     return Xout;
 }
 
-freealg subs(const freealg X, const freealg Y, const NumericVector r){
+freealg subs(const freealg& X, const freealg& Y, const NumericVector r){
     freealg out;
 
     // We know the words of X have no no zeros, so first we substitute
