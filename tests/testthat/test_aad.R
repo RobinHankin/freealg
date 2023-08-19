bea315b64249b744f0757ec20320cd56cc6eaea7
Robin Hankin <hankin.robin@gmail.com>
2023-08-19 15:22:21 +1200

    whitespace

--- tests/testthat/test_aad.R
+++ tests/testthat/test_aad.R
@@ -1,5 +1,3 @@
-
-
 test_that("test suite aad",{
   subs("a",a="1+x") == as.freealg("1+x")^1
   subs("aa",a="1+x") == as.freealg("1+x")^2
@@ -8,8 +6,6 @@ test_that("test suite aad",{
 
   f <- function(n){expect_true(subs(as.freealg(paste(rep("a",n),collapse="")),a="1+x") == as.freealg("1+x")^n)}
   sapply(1:20,f)
-
-
 
   foo <- function(p){
     if(!(24 %in% unique(c(elements(words(p)),recursive=TRUE)))){
@@ -20,7 +16,6 @@ test_that("test suite aad",{
   for(i in 1:10){
     foo(rfalg(distinct=3))
   }
-
 
   expect_true(linear(1:3) == as.freealg('a+2b+3c'))
   expect_false(linear(1:3) == as.freealg('1+a+2b+3c'))
