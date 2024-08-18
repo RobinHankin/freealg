91a43704806da120fe0c6c5278c00e86e19f6978
robin <hankin.robin@gmail.com>
2024-08-18 17:40:40 +0100

    remove incorrect docs

--- tests/testthat/test_aag.R
+++ tests/testthat/test_aag.R
@@ -1,10 +1,5 @@
 ## This file follows the structure of aaa.R in the free group package.
 
-## Define some checker functions, and call them at the end.  They
-## should all return TRUE if the package works, and stop with error if
-## a test is failed.  Function checker1() has one argument, checker2()
-## two, and checker3() has three.  
-
 test_that("Test suite aag.R",{
 
   expect_true(as.freealg("-1 + x") == -as.freealg("1-x"))
